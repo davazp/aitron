4f002d392ae9801b0b964aabb6ed85a92bd2fa2e
David Vázquez <davazp@gmail.com>
2011-12-23 11:48:05 +0000

    Movimiento maximizando el area disponible

--- boa.lisp
+++ boa.lisp
@@ -164,12 +164,16 @@
     gradient))
 
 
-(defun list-movements (i j)
+(defun list-movements (i j &optional (null-movement-p t))
   (loop for (di dj) in '((-1 0) (1 0) (0 -1) (0 1))
         for ni = (+ i di)
         for nj = (+ j dj)
         when (and (valid-cell-p ni nj) (free-cell-p ni nj))
-        collect (list di dj)))
+        collect (list di dj) into result
+        finally
+           (if (and (null result) null-movement-p)
+               (return (list '(1 0)))
+               (return result))))
 
 (defun list-neighbours (i j)
   (loop for (di dj) in '((-1 0) (1 0) (0 -1) (0 1))
@@ -178,13 +182,96 @@
         when (and (valid-cell-p ni nj) (free-cell-p ni nj))
         collect (list ni nj)))
 
-(defun minimize (list predicate &key (key #'identity))
-  ;; Aqui nos podemos ahorrar bastante computación
+(defun optimizing (list predicate &key (key #'identity))
+  ;; TODO: Better implementation
   (reduce (lambda (a b)
             (if (funcall predicate (funcall key a) (funcall key b))
                 a
                 b))
           list))
+
+(defun minimizing (list &key (key #'identity))
+  (optimizing list #'< :key key))
+
+(defun maximizing (list &key (key #'identity))
+  (optimizing list #'> :key key))
+
+
+;;; Move to the cell nearest I,J according to the real distance.
+(defmacro destructuring-lambda (lambda-list &body body)
+  (let ((args (gensym)))
+    `(lambda (&rest ,args)
+       (destructuring-bind ,lambda-list ,args ,@body))))
+
+(defun move-to (i j)
+  (let ((grad (compute-gradient i j))
+        (movs (list-movements (player-i *me*) (player-j *me*))))
+    (apply #'move
+           (minimizing (scramble movs)
+                       :key (destructuring-lambda ((di dj))
+                              (aref grad
+                                    (+ di (player-i *me*))
+                                    (+ dj (player-j *me*))))))))
+
+
+(defun curry (fn &rest preargs)
+  (lambda (&rest postargs)
+    (apply fn (append preargs postargs))))
+
+(defun rcurry (fn &rest postargs)
+  (lambda (&rest preargs)
+    (apply fn (append preargs postargs))))
+
+;;; TODO: PROBAR
+(defun maparray (f array &rest others)
+  (let* ((dimensions (array-dimensions array))
+         (result (make-array dimensions))
+         (arrays (cons array others)))
+    (dotimes (i (array-total-size array))
+      (let ((value (apply f (mapcar (rcurry #'row-major-aref i) arrays))))
+        (setf (row-major-aref result i) value)))))
+
+(defun compare (a b)
+  (cond
+    ((< a b) -1)
+    ((= a b)  0)
+    ((> a b)  1)))
+
+(defun linearize-array (array)
+  (make-array (array-total-size array) :displaced-to array))
+
+(defun compute-fill-algorithm (i j)
+  (let* ((grad (linearize-array (compute-gradient i j)))
+         (n (length grad)))
+    (- n (count n grad))))
+
+
+;;; Move to the maximizing fill area cell
+(defun move-to-maximize-fill ()
+  (apply #'move (maximizing (scramble (list-movements (player-i *me*) (player-j *me*)))
+                            :key (lambda (mov)
+                                   (let ((i (+ (player-i *me*) (first mov)))
+                                         (j (+ (player-j *me*) (second mov))))
+                                     (let ((value (compute-fill-algorithm i j)))
+                                       (prog1 value
+                                         (log "COMPUTE-FILL: ~d ~d => ~d" i j value))))))))
+
+
+;;; LAS TECNICAS QUE VOY IMPLEMENTANDO ARRIBA SON INDIVIDUALES, Y EN
+;;; GENERAL SE TRATA DE OPTIMIZAR UNA FUNCION VALOR. TENER ESTO EN
+;;; CUENTA PARA CUANDO REORGANIZE EL CODIGO, HACIENDOLO MUCHO MAS
+;;; GENERICO Y PERMITIENDO COMBINAR FUNCIONES DE VALORES COMODAMENTE,
+;;; QUIZA CON PARAMETROS.
+;;;
+;;; IGUALMENTE, APLICAR EL VALOR NO A VECINOS INMEDIATOS, SINO A UNA
+;;; VECINIDAD MAYOR Y/O PUNTOS ESTRATEGICOS DEL TABLERO.
+;;;
+;;; EN LA COMBINACION DE ESTAS ESTRATEGIAS (MINIMAX PAR CERCA),
+;;; INTERVIENE LA DISTANCIA COMO PARAMETRO TAMBIEN.
+;;;
+;;; IGUALMENTE, UNA VEZ EL OBJETIVO DE SABER CUANDO EL ENEMIGO ESTA
+;;; ENCERRADO ES RELLENAR EL MAYOR AREA POSIBLE, ESTO ES HECHO CON
+;;; METODOS TRADICIONALES.
 
 
 (defun main ()
@@ -204,15 +291,12 @@
   (destructuring-bind (n &optional ign) (read-cords)
     (declare (ignore ign))
     (loop repeat n do (apply #'set-cell-as-busy (read-cords))))
-  (let ((grad (compute-gradient 0 0)))
+  (let ((*random-state* (make-random-state t)))
     (loop
-      (apply #'move
-       (minimize (scramble (or (list-movements (player-i *me*) (player-j *me*)) '((1 0)))) #'<
-                 :key (lambda (mov)
-                        (destructuring-bind (di dj) mov
-                          (aref grad
-                                (+ di (player-i *me*))
-                                (+ dj (player-j *me*)))))))
+      (let ((movs (list-movements (player-i *me*) (player-j *me*))))
+        (if (= (length movs) 4)
+            (apply #'move (car (scramble movs)))
+            (move-to-maximize-fill)))
       (apply #'set-cell-as-busy (read-cords)))))
 
 ;;; boa ends here
